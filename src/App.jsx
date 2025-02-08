d2e450c5106357d1c64a694348dc283276e53a38
eliptik <adamfeyk5@gmail.com>
2025-02-08 09:12:06 +0300

    Set up Vite + TailwindCSS

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-5xl">Google Flights</h1>
+      <div className=""></div>
+    </>
+  );
+}
+
+export default App;
